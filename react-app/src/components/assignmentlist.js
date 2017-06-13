06e39d5315571cff38dfef3ba37c0c3c3205ca23
Alex <alexander@sapozhkov.net>
2017-06-14 01:26:35 +0300

    more assignments

--- react-app/src/components/assignmentlist.js
+++ react-app/src/components/assignmentlist.js
@@ -1,29 +1,90 @@
 import React, { Component } from 'react';
 import './assignmentlist.css';
 import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
+import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
 
 
 class AssignmentList extends Component {
     constructor(props) {
         super(props);
+        var roleFilters = {};
+        for (let role of this.props.assignmentListData.roles) {
+            roleFilters[role] = true;
+        }
+        var technologyFilters = {};
+        for (let technology of this.props.assignmentListData.technologies) {
+            technologyFilters[technology] = true;
+        }
         this.state = {
             featuredOnly: true,
             scrollTop: 0,
             showHeader: false,
+            roleFilters: roleFilters,
+            technologyFilters: technologyFilters
         };
     };
     render() {
-        var filteredAssignmentListDataItems = this.props.assignmentListData.items
+        var roleFilters = this.props.assignmentListData.roles.map((role, index)=>  
+            <div className="rolefilter-col ms-Grid-col ms-u-sm6 ms-u-md4 ms-u-lg3 ms-u-xl2" key={index}>
+                <Checkbox
+                    label={role}
+                    checked={ this.state.roleFilters[role] }
+                    onChange={ (ev, checked) => {
+                        var newroleFilters = {};
+                        for (var key in this.state.roleFilters)
+                        {
+                            newroleFilters[key] = key===role ? checked : this.state.roleFilters[key]
+                        }
+                        this.setState({ roleFilters: newroleFilters })
+                    }}
+                    />
+            </div>
+        );
+        var technologyFilters = this.props.assignmentListData.technologies.map((technology, index)=>  
+            <div className="technologyfilter-col ms-Grid-col ms-u-sm6 ms-u-md4 ms-u-lg3 ms-u-xl2" key={index}>
+                <Checkbox
+                    label={technology}
+                    checked={ this.state.technologyFilters[technology] }
+                    onChange={ (ev, checked) => {
+                        var newtechnologyFilters = {};
+                        for (var key in this.state.technologyFilters)
+                        {
+                            newtechnologyFilters[key] = key===technology ? checked : this.state.technologyFilters[key]
+                        }
+                        this.setState({ technologyFilters: newtechnologyFilters })
+                    }}
+                    />
+            </div>
+        );
+        var filteredAssignmentListDataItems = this.props.assignmentListData.items;
         if (this.state.featuredOnly) filteredAssignmentListDataItems = filteredAssignmentListDataItems.filter(function (item) {
             return (item.featured === "true")
-            });
+        });
+        var roleFiltersState = this.state.roleFilters;
+        filteredAssignmentListDataItems = filteredAssignmentListDataItems.filter(function (item){
+            var display = false;
+            for (var role in roleFiltersState)
+            {
+                if ( roleFiltersState[role] && item.roles.includes(role) ) display = true;
+            }
+            return display;
+        });
+        var technologyFiltersState = this.state.technologyFilters;
+        filteredAssignmentListDataItems = filteredAssignmentListDataItems.filter(function (item){
+            var display = false;
+            for (var technology in technologyFiltersState)
+            {
+                if ( technologyFiltersState[technology] && item.technologies.includes(technology) ) display = true;
+            }
+            return display;
+        });
         var assignments = filteredAssignmentListDataItems.map((assignmentData, index)=>
             <div className="projectitem-col ms-Grid-col ms-u-sm12 ms-u-lg6 ms-u-xl4 ms-u-xxl3 ms-u-fadeIn500" key={index}>
                 <div className="ms-Grid assignmentitem-grid">
                     <div className="ms-Grid-row">
-                        <div className="assignmenttitle-col assignmentvalue-col ms-Grid-col ms-fontWeight-semibold ms-u-sm12">
+                        <h4 className="assignmenttitle-col assignmentvalue-col ms-Grid-col ms-fontWeight-semibold ms-u-sm12">
                             {assignmentData.title}
-                        </div>
+                        </h4>
                         <div className="assignmentlogo-col assignmentvalue-col ms-Grid-col ms-u-sm12">
                             <img src={require('../photos/'+assignmentData.clientLogo)} className="assignmentlogo" alt={assignmentData.title}/>
                         </div>
@@ -54,9 +115,29 @@ class AssignmentList extends Component {
                 <div className="listfilter">
                     <Toggle
                         defaultChecked={ this.state.featuredOnly }
-                        label='Show featured projects only'
+                        label={ this.props.assignmentListData.featuredFilterText }
                         onChanged={(checked) => {this.setState({featuredOnly: checked})}}
                         />
+                    <div className="rolefilter-area">
+                        <h3>{
+                            this.props.assignmentListData.roleFiltersHeaderText }
+                        </h3>
+                        <div className="ms-Grid rolefilter-grid">
+                            <div className="ms-Grid-row">
+                                {roleFilters}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="technologyfilter-area">
+                        <h3>
+                            { this.props.assignmentListData.technologyFiltersHeaderText }
+                        </h3>
+                        <div className="ms-Grid technologyfilter-grid">
+                            <div className="ms-Grid-row">
+                                {technologyFilters}
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="ms-Grid assignmentlist-grid">
                     <div className="ms-Grid-row">
